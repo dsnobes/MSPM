a59a98f443faf48a13c5ecfeb34c229cc6b2f194
Cedric Boucher <onebitalpha@gmail.com>
2023-05-17 13:49:17 -0600

    fixed indentation

--- 01_Latest_MSPM_Version/v1,9,2_Matthias_2022_12/v1,9,2/Function - Turb Nusselt/AlignedTubeBankConduction.m
+++ 01_Latest_MSPM_Version/v1,9,2_Matthias_2022_12/v1,9,2/Function - Turb Nusselt/AlignedTubeBankConduction.m
@@ -1,9 +1,9 @@
 function [ Const, Exponent ] = AlignedTubeBankConduction( Xt,Xl,do )
-%ALIGNEDTUBEBANKCONDUCTION Const * Re ^ Exponent = Nst*Npr^(2/3)
-Xt_Xl = Xt/Xl;
-Xt_do = Xt/do;
-Xl_do = Xl/do;
-Const = (0.118*Xt_Xl+0.252);
-Exponent = (-0.0125*Xt_Xl-0.433*Xl+0.0765*Xt-0.0892);
+    %ALIGNEDTUBEBANKCONDUCTION Const * Re ^ Exponent = Nst*Npr^(2/3)
+    Xt_Xl = Xt/Xl;
+    Xt_do = Xt/do;
+    Xl_do = Xl/do;
+    Const = (0.118*Xt_Xl+0.252);
+    Exponent = (-0.0125*Xt_Xl-0.433*Xl+0.0765*Xt-0.0892);
 end
 
