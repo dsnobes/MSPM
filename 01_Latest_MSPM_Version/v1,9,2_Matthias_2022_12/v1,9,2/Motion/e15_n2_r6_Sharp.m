8a2595fc49df95e95e596cc7b1425e371273916f
FalsettiM <90410065+FalsettiM@users.noreply.github.com>
2023-06-26 11:44:54 -0600

    Deleted the motion folder as it is not used anymore Issue #29

--- 01_Latest_MSPM_Version/v1,9,2_Matthias_2022_12/v1,9,2/Motion/e15_n2_r6_Sharp.m
+++
@@ -1,9 +0,0 @@
-function [ pos ] = e15_n2_r6_Sharp( Nang, Phase )
-    Phase = Phase + pi;
-    e = 1/5;
-    n = 2;
-    con_cr_ratio = 6;
-    TransferPhase = 0;
-    pos = Ellipsoidal(Nang,Phase,e,n,con_cr_ratio,TransferPhase);
-end
-
