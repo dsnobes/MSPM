95b8168f4403831b1079847ebb73923d33e4b080
Cedric Boucher <onebitalpha@gmail.com>
2023-07-04 10:01:59 -0600

    consolidated if/elseif/else

--- 01_Latest_MSPM_Version/v1,9,2_Matthias_2022_12/v1,9,2/Run.m
+++ 01_Latest_MSPM_Version/v1,9,2_Matthias_2022_12/v1,9,2/Run.m
@@ -1,3 +1,10 @@
+%{
+Run this file to start MSPM!
+this clears memory, runs a startup script which ensures
+all necessary subdirectories are added to MATLAB's PATH,
+and starts the GUI (which takes care of everything else)
+%}
+
 clear; clc;
 startup();
 SimulationInterfaceV5();
