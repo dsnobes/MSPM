5b6a9b6bff070c989e151ea469042daefab5ea79
Cedric Boucher <onebitalpha@gmail.com>
2023-07-06 14:48:40 -0600

    fixed error get volume of empty model

--- 01_Latest_MSPM_Version/MSPM_2023/GUI/SimulationInterfaceV5.m
+++ 01_Latest_MSPM_Version/MSPM_2023/GUI/SimulationInterfaceV5.m
@@ -2173,6 +2173,11 @@ for j = 1:length(h.Model.Groups)
     end
 end
 
+if pos == 1
+    % no bodies, would have error below
+    disp(['The total volume (excl. HX and regenerator) is: ', num2str(0), ' L'])
+    return
+end
 % Go through all the volumes and add them
 vol_table = struct2table(volumes);
 total_vol = sum(vol_table.vol);
